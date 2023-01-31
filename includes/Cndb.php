c3887e24e3e95bc6d11a2b47d73fde8dd76ffd9e
Soufian Bouktaib <109437519+sofexbk@users.noreply.github.com>
2023-01-31 21:00:49 +0100

    Update Cndb.php

--- includes/Cndb.php
+++ includes/Cndb.php
@@ -2,7 +2,7 @@
 $dbservername="localhost";
 $dbusername="Sos";
 $dbpassword="18901890";
-$dbname="ensat";
+$dbname="ensatdata";
 $conn=mysqli_connect("localhost","Sos","18901890","ensatdata");
 if(!$conn){
     die(mysqli_error($conn));
